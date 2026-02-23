8c399fef09ded9ca622aaf82a98f9b2d2bba5627
Ardion MassaId <ardionm97@gmail.com>
2026-02-23 16:19:58 +0700

    Add: add icon dekstop app

--- composeApp/src/jvmMain/kotlin/com/bumi/app/main.kt
+++ composeApp/src/jvmMain/kotlin/com/bumi/app/main.kt
@@ -3,6 +3,8 @@ package com.bumi.app
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import bumiapp.composeapp.generated.resources.Res
+import bumiapp.composeapp.generated.resources.ic_bumi
 import com.bumi.app.di.initKoin
 import org.jetbrains.compose.resources.painterResource
 
@@ -11,7 +13,8 @@ fun main() {
     application {
         Window(
             onCloseRequest = ::exitApplication,
-            title = "BumiApp"
+            title = "BumiApp",
+            icon = painterResource(Res.drawable.ic_bumi)
         ) {
             App()
         }
